f52b5d3d783d775ce70aebd9af043de880bf1605
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-07-08 01:24:55 -0400

    first commit

---
+++ src/main/java/com/minhhoangtran/springrestapi/SpringRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.minhhoangtran.springrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestapiApplication.class, args);
+	}
+
+}
